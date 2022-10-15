3e3557c96e565978586eedb6ed49182d8a5912d1
Gunnar Geir <gunnargeir89@gmail.com>
2022-10-15 16:18:31 +0000

    Hello

--- lib/functions/status.dart
+++ lib/functions/status.dart
@@ -16,8 +16,8 @@ hand before showdown.)
  */
 void Status(List<int> playerHand, List<int> houseHand){
 
-    print('Player\'s hand is ${CardNameList(playerHand)}${CardNameList(playerHand)} . His score is ${CalculateScore(playerHand)}.');
-    print('House\'s card is ${CardNamer(houseHand[0])}${CardNamer(houseHand[1])}.');
+    print('Player\'s hand is ${CardNamer(playerHand[0])}${CardNamer(playerHand[1])}. His score is ${CalculateScore(playerHand)}.');
+    print('House\'s card is ${CardNamer(houseHand[0])} ${CardNamer(houseHand[1])}. His score is ${CalculateScore(houseHand)}.');
   }
   List<String> CardNameList(List<int> playerHand) {
     List<String> names = [];
