a6691b358d9a8481c6fad4216f1f9c66ddb5e3a8
Gunnar Helgason <gunnarh@ntv.is>
2022-10-15 15:16:37 +0000

    Fixing conflict

--- bin/BlackJack.dart
+++ bin/BlackJack.dart
@@ -6,15 +6,9 @@ import 'package:BlackJack/functions/welcome.dart';
  */
 
 void main() {
-<<<<<<< HEAD
   Welcome();
 
   List<int> deck = ShuffledDeck();
   List<int> PlayerHand = [];
-  List<int> HouseHand = []
-=======
-  List <int> playerHand = [2,5];
-  List <int> houseHand = [3,3];
-  Status(playerHand, houseHand);
->>>>>>> 63f61dbe9d3dff26eca018e996ea27cc988a4cf4
+  List<int> HouseHand = [];
 }
