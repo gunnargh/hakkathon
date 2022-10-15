c282aaa4de34cb6daf6e7150fa8aa50b369a68ec
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-10-15 15:32:26 +0000

    is it this?

--- lib/functions/initial_deal.dart
+++ lib/functions/initial_deal.dart
@@ -22,7 +22,9 @@ Added to make it work
 
 import 'draw_card.dart';
 
-void InitialDeal(List<int> playerHand, List<int> houseHand, List<int> deck){
+void InitialDeal(List<int> playerHand, List<int> houseHand, List<int> deck) {
+ DrawCard(playerHand, deck);
+ DrawCard(houseHand, deck);
  DrawCard(playerHand, deck);
  DrawCard(houseHand, deck);
 }
